e9c9fb8379f8a00de938fb8f4706a262cd537623
fortes42 <forteslv42@gmail.com>
2025-11-07 13:23:42 +0900

    settings

---
+++ movie_home/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:perfectionist/recommended-natural',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'jsx-a11y', 'perfectionist'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
